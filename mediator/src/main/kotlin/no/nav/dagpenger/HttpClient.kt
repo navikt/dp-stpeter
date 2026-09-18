0e9e9a7fbf89c03bb3ccf6154a6fb2db08288950
fsjovatsen <frode.sjovatsen@nav.no>
2026-09-18 13:30:57 +0200

    Lagt til timeout setting for http kall mot tilgangsmaskinen

--- mediator/src/main/kotlin/no/nav/dagpenger/HttpClient.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/HttpClient.kt
@@ -4,11 +4,13 @@ import io.ktor.client.HttpClient
 import io.ktor.client.HttpClientConfig
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.http.ContentType
 import io.ktor.serialization.jackson3.JacksonConverter
 import io.prometheus.metrics.model.registry.PrometheusRegistry
 import no.nav.dagpenger.ktor.client.metrics.PrometheusMetricsPlugin
+import kotlin.time.Duration.Companion.seconds
 
 fun createHttpClient(
     prometheusRegistry: PrometheusRegistry = PrometheusRegistry.defaultRegistry,
@@ -25,6 +27,11 @@ fun createHttpClient(
     install(ContentNegotiation) {
         register(ContentType.Application.Json, JacksonConverter(objectMapper))
         register(ContentType.Application.ProblemJson, JacksonConverter(objectMapper))
+    }
+    install(HttpTimeout) {
+        requestTimeoutMillis = 5.seconds.inWholeMilliseconds
+        connectTimeoutMillis = 2.seconds.inWholeMilliseconds
+        socketTimeoutMillis = 3.seconds.inWholeMilliseconds
     }
     configure()
 }
